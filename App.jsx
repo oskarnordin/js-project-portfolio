d64a4b7acd8310eff3695933f99bc079e01aa0c9
oskarnordin <oskarnordin1@gmail.com>
2025-04-28 19:26:51 +0200

    Update app.jsx
    removed style.css

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import HeroVideo from "./components/HeroVideo";
 import FeaturedProjects from "./components/Sections/Projects";
 import MyWordsSection from "./components/Sections/MyWords";
 import ContactSection from "./components/Sections/Contact";
-import "./style.css";
+// import "/style.css";
 
 function App() {
   return (
